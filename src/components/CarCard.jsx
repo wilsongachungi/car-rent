3995bf3d681e2a583c1a88752a56beeabe30504a
wilsongachungi <gachungiwilson91@gmail.com>
2025-09-08 14:39:28 +0300

    creating featured cars section

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { useNavigate} from 'react-router-dom'
 
 const CarCard = ({car}) => {
     const currency = import.meta.env.VITE_CURRENCY;
+    const navigate = useNavigate();
   return (
-    <div className='group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer'>
+    <div onClick={()=> {navigate(`/car-details/${car._id}`); scrollTo(0,0)}} className='group rounded-xl overflow-hidden shadow-lg hover:-translate-y-1 transition-all duration-500 cursor-pointer'>
       <div className='relative h-48 overflow-hidden'>
         <img src={car.image} alt="Car Image"  className='w-full object-cover transition-transform duration-500 group-hover:scale-105'/>
         {car.isAvailable} && <p className='absolute top-4 left-4 bg-primary/90 text-white text-xs px-2.5 py-1 rounded-full'>Available Now</p>
---
+++ src/components/FeaturedSection.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import Title from './Title'
+import { dummyCarData, assets } from '../assets/assets'
+import CarCard from '../components/CarCard'
+
+const FeaturedSection = () => {
+  const navigate = useNavigate()
+
+  return (
+    <div className='flex flex-col items-center py-24 px-6 md:px-16 lg:px-24 xl:px-32'>
+      <div>
+        <Title
+          title='Featured Vehicles'
+          subTitle='Explore our selection of premium vehicles available for your next adventure'
+        />
+      </div>
+
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-16'>
+        {dummyCarData.slice(0, 6).map((car) => (
+          <div key={car._id}>
+            <CarCard car={car} />
+          </div>
+        ))}
+      </div>
+
+      <button
+        onClick={() => {
+          navigate('/cars')
+          window.scrollTo(0, 0)
+        }}
+        className='flex items-center justify-center gap-2 px-6 py-2 border border-borderColor hover:bg-gray-50 rounded-md mt-16 cursor-pointer'
+      >
+        Explore all cars <img src={assets.arrow_icon} alt='arrow' />
+      </button>
+    </div>
+  )
+}
+
+export default FeaturedSection
